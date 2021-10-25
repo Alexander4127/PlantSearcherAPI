a3905c526fa789f8cce118f17f5286fff02cee47
Alexander4127 <71152283+Alexander4127@users.noreply.github.com>
2021-10-25 18:53:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ plantFinderByParam = PlantFinder()
 generatorWeed = RandomWeedInfo()
 
 
-# здесь я повторяю пример с лекции
 @app.route("/form")
 def form():
     return render_template("form.html")
@@ -17,25 +16,21 @@ def form():
 def data():
     if request.method == "GET":
         return (
-            f"Сначала проследуйте по пути /form для ввода данных"
+            f"Please, choose the path /form"
         )
     if request.method == "POST":
-        # получаем название или типы и цвет растения из запроса и
-        # вызываем метод __call__()
         plant_name, plant_types, plant_colour = \
             request.form['plant_name'], request.form.getlist('plant_types'), request.form.get('plant_colour')
 
         form_data = plantFinderByParam(plant_types, plant_colour, plant_name)
         if form_data is None:
-            return f"К сожалению, такое растение не найдено"
+            return f"Plant has not found"
 
-        # генерируем случайный сорняк, затем добавляем информацию
-        # о нём в словарь
         weed = generatorWeed()
-        form_data['weed_name'] = weed['Название'].values[0]
-        form_data['weed_info'] = weed['Информация'].values[0]
-        form_data['weed_photo'] = weed['Фото'].values[0]
-        form_data['weed_link'] = weed['Ссылка на страницу'].values[0]
+        form_data['weed_name'] = weed['Name'].values[0]
+        form_data['weed_info'] = weed['Info'].values[0]
+        form_data['weed_photo'] = weed['Photo'].values[0]
+        form_data['weed_link'] = weed['Link'].values[0]
 
         return render_template("data.html", form_data=form_data)
 
